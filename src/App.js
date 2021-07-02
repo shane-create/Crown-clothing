d4fafde33645d64921b556e083b56ccaabdd5a6b
xhane-create <shai0136@g2650.dk>
2021-07-02 16:39:18 +0200

    Hey! Sot that withrouter function is. Also i created another selecteor for the users. I also used createStructura thousand times.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import {connect} from "react-redux";
 
 import './App.css';
 import {Switch, Route, Redirect} from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
 
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 
 import {auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import reactDom from 'react-dom';
 import {setCurrentUser} from "./redux/user/user.actions";
-
+import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends React.Component {
   // constructor(props){
@@ -53,7 +55,8 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/shop" component={ShopPage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
         </Switch>
       </div>
@@ -61,8 +64,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import {connect} from "react-redux";
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from "react-router";
 
-import FlexibleButton from "../flexible-button/flexible-button.component";
-import CartItem from "../cart-item/cart-item.component"
 import { selectCartItems } from "../../redux/cart/cart.selectors";
+import CartItem from "../cart-item/cart-item.component"
+import FlexibleButton from "../flexible-button/flexible-button.component";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({cartItems}) => (
+const CartDropdown = ({cartItems, history}) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {
-                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+                cartItems.length ?
+                cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem} />))
+                :
+                <span className="empty-message">Your cart is empty!</span>
             }
         </div>
-        <FlexibleButton>GO TO CHECK OUT</FlexibleButton>
+        <FlexibleButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</FlexibleButton>
     </div>
 )
 
-const mapStateToProps = state => ({
-    cartItems: selectCartItems(state)
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems
 })
 
 //There are some styles in index.css!!!
 
-export default  connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));

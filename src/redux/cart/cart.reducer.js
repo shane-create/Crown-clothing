c1708b064ac4421960becf5fc32ccbe6a708dbf4
xhane-create <shai0136@g2650.dk>
2021-07-05 16:01:16 +0200

    I have now added some code so that you can remove items! You can also increase and decrease the amount of items in the checkout page. If the current quantity of an item is 1 and you decrease, then it will remove the item instead. Personally, I think it would be better if you didn't allow the user to decrease below 1, so they would have to click the romove button, because what if the accidently cklick decrease to many times? That would be annoying!

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
-import CartActionType from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import CartActionTypes from "./cart.types";
+import { addItemToCart, removeItemFromCart} from "./cart.utils";
 
 const INITIAL_STATE = {
     hidden: true,
@@ -8,15 +8,25 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch(action.type){
-        case CartActionType.TOGGLE_CART_HIDDEN:
+        case CartActionTypes.TOGGLE_CART_HIDDEN:
             return{
                 ...state,
                 hidden: !state.hidden
             }
-        case CartActionType.ADD_ITEM:
+        case CartActionTypes.ADD_ITEM:
             return{
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+        case CartActionTypes.REMOVE_ITEM:
+            return{
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return{
+                ...state,
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
             }
 
         default:
